e4b36c1277c66a6b05f3138c772cd5eeddf3655c
solomon-ctg <solomon.iroegbu@gmail.com>
2025-01-08 20:12:08 +0100

    feat: eShop Header

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+//import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import Header from "./Header";
+
+
+function App() {
+  return (
+    <div className="App">
+
+	    <Header />
+
+    </div>
+  );
+}
+
+export default App;
